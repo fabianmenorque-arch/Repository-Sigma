e59e608c179303ef6d12574170df23faa38ebbfd
fabianmenorque-arch <fabianmenorque@gmail.com>
2026-06-18 01:42:10 -0300

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+PROJECT_NAME = "SIGMA"
+
+VERSION = "0.1.0-alpha"
+
+DATABASE_FOLDER = "database"
+
+DATA_FOLDER = "data"
+
+LOG_FOLDER = "logs"
+
+EXCEL_FILENAME = "Base de datos, Máquinas.xlsx"
+
+REGISTRY_FILENAME = "asset_registry.json"
